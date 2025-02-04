9a900d378424d2dcb4c4079f78f80372a7ed7b3c
Tania <Taniachumberofficial@gmail.com>
2025-02-04 11:23:29 -0500

    Fixed minor errors

--- Data/NIACRMigration/20250203082816_Initial.Designer.cs
+++ Data/NIACRMigration/20250203082816_Initial.Designer.cs
@@ -11,7 +11,7 @@ using NIA_CRM.Data;
 namespace NIA_CRM.Data.NIACRMigration
 {
     [DbContext(typeof(NIACRMContext))]
-    [Migration("20250203055910_Initial")]
+    [Migration("20250203082816_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
